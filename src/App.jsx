4d7625c2f984ea6961c0d98a607e8b67b0de19cb
Sookyuu <max.roiron@epitech.eu>
2026-04-29 09:44:42 +0200

    {ADD} all 10 questions + css simplification

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,63 @@
 import {useState } from "react";
 
-const quizData = [
+const quizQuestions = [
   {
-    question: "Quelle est ta couleur préférée ?",
+    question: "Question 1\nQuelle est ta couleur préférée ?",
     choices: ["Le violet", "Le bordeaux", "Le magenta", "Le noir"],
-    answer: 0,
+    answer: 1,
+    reponse: "Je le savais !!"
   },
   {
-    question: "Question 2 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
+    question: "Question 2\nQuel est ton plat préféré?",
+    choices: ["Les pâtes carbos", "La tartiflette au crozet de Stéphanie", "Max (grr)", "Autre (putain max tu connais pas ta copine c'est chaud)"],
+    answer: 2,
+    reponse: "Evidemment que c'est ça, quoi d'autre."
   },
   {
-    question: "Question 3 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
+    question: "Question 3\nTu préfères...?",
+    choices: ["Te noyer dans l'océan", "Te noyer dans un aquarium mais il est aussi grand que l'océan",
+      "Te noyer dans un lac mais c'est comme un aquarium", "Te noyer dans une rivière mais ya pas de courant et c'est comme un aquarium"],
+    answer: 3,
+    reponse: "Un choix pas facile"
   },
   {
-    question: "Question 4 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
+    question: "Question 4\nPour se murger la gueule, tu prends quoi ?",
+    choices: ["Sous vody puff 25k PURPLE WAVE tasty crousty", 
+      "Du vin", "Du vin Blanc", "Du vin rouge"],
     answer: 0,
+    reponse: "Dans la france à macron???"
   },
   {
-    question: "Question 5 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
+    question: "Question 5\nQuel est ton animal préféré ?",
+    choices: ["Le poulpe", "Le chat", "La Musaraigne d'Éthiopie", "Le renard"],
+    answer: 3,
+    reponse: "Le savais-tu : La Musaraigne d'Éthiopie cache une petite particularité étonnante malgré sa taille minuscule. On a observé que certaines musaraignes de cette espèce peuvent émettre des sons ultrasoniques pour s’orienter dans leur environnement, un peu comme les chauves-souris. Ce n’est pas une “écholocation” aussi sophistiquée, mais cela leur permettrait de mieux naviguer dans les herbes denses ou les terriers sombres où elles vivent."
+
   },
   {
-    question: "Question 6 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
+    question: "Question 6 7 SIIIXXX SEVEEEEENNN\nQuel est ton brainrot préféré?",
+    choices: ["Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Tung Sahur",
+      "Brim Brim Paftapim", "Chimpanzini BANANINI", "Réponse D"],
     answer: 0,
+    reponse: "zig & sharko"
   },
   {
-    question: "Question 7 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
+    question: "Dans cette situtation, Je..",
+    choices: ["Ralentis", "Garde mon allure", "J'accèlere", "J'ai même pas mon code qu'est ce que tu me racontes fdp"],
+    answer: 3,
+    reponse: "ébédidonk"
   },
   {
-    question: "Question 8 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
+    question: "C'est la question CO ! C'est la question QUINE !\nC'est la Queeeessssstioooon COOOOO QUINEEEE!!\nTu préfères...?",
+    choices: ["Le 69", "Le 67 (SIIIXX SEVVEENN)", "Ligoté mains/pieds, suspendu, bailloné", "Réponse D"],
+    answer: 3,
+    reponse: "Coquine va"
   },
   {
-    question: "Question 9 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
-  },
-  {
-    question: "Question 10 ?",
-    choices: ["Réponse A", "Réponse B", "Réponse C", "Réponse D"],
-    answer: 0,
+    question: "Question 10\nQuelle prénom choisirais-tu pour notre futur chat ?",
+    choices: ["Tulip", "Pignouf", "MÉGATRON", "Salaud"],
+    answer: 2,
+    reponse: "par pitié mégatron"
   },
 ];
 
@@ -116,7 +124,10 @@ function PageMood({ navigateTo }) {
           </div>
         ))}
       </div>
-
+      <div className="images-row">
+        <img src="photos/happyy.jpg" alt="happyy" />
+        <img src="photos/sadd.jpg" alt="sadd" />
+      </div>
       <button
         className="arrow-btn"
         onClick={() => {
@@ -134,12 +145,11 @@ function PageMood({ navigateTo }) {
 function PageVeryGood() {
   return (
     <div className="page" id="page-very-good">
-      <h1 className="text_title">Trop bien !</h1>
-      <img
-        src="photos/very_good_dog.jpg"
-        alt="very good dog"
-        style={{ width: "200px", height: "auto" }}
-      />
+      <h1 className="text_title">Weeeeeeeee</h1>
+      <div className="images-row">
+        <img src="photos/patrick.jpg" alt="patrick" />
+        <img src="photos/happy1.jpg" alt="happy1" />
+      </div>
     </div>
   );
 }
@@ -147,10 +157,10 @@ function PageVeryGood() {
 function PageOk() {
   return (
     <div className="page" id="page-ok">
-      <h1 className="text_title">Ça va ça va</h1>
+      <h1 className="text_title">mhmhmh ok ok !</h1>
       <img
-        src="photos/ok_dog.jpg"
-        alt="ok dog"
+        src="photos/okok.jpg"
+        alt="okok"
         style={{ width: "200px", height: "auto" }}
       />
     </div>
@@ -160,7 +170,7 @@ function PageOk() {
 function PageBof() {
   return (
     <div className="page" id="page-bof">
-      <h1 className="text_title">Bof je suis fatigué j'ai pas trop dormi</h1>
+      <h1 className="text_title">A MIMIR</h1>
       <img
         src="photos/bof_dog.jpg"
         alt="bof dog"
@@ -173,10 +183,15 @@ function PageBof() {
 function PageBridge() {
   return (
     <div className="page" id="page-bridge">
-      <h1 className="text_title">Ça va pas jvais sauter d'un pont</h1>
+      <h1 className="text_title">OMG NON PAS LE PONT !!!</h1>
       <img
-        src="photos/bridge_dog.jpg"
-        alt="bridge dog"
+        src="photos/bridge.jpeg"
+        alt="bridge"
+        style={{ width: "200px", height: "auto" }}
+      />
+      <img
+        src="photos/scream_dog.jpg"
+        alt="scream_dog"
         style={{ width: "200px", height: "auto" }}
       />
     </div>
